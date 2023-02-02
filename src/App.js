1c351582b8cece86d0eea38c97e03da91921778c
Alex <vanguard88728@gmail.com>
2023-02-02 17:35:55 +0800

    Configure route

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { StyledEngineProvider } from '@mui/material';
+
+import Home from './assets/pages/Home';
 import './App.css';
 
-function App() {
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <StyledEngineProvider injectFirst>
+        <BrowserRouter>
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </StyledEngineProvider>
     </div>
   );
 }
