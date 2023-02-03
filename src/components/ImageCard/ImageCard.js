1664dce11d18343849a042e3b730951b0716093b
Alex <vanguard88728@gmail.com>
2023-02-03 09:47:18 +0800

    Create ImageCard component

---
+++ src/components/ImageCard/ImageCard.js
@@ -0,0 +1,29 @@
+import './ImageCard.css';
+
+import TopImage from '../../assets/images/CardTopHolder.png';
+import BottomImage from '../../assets/images/CardBottomHolder.png';
+
+const CardItem = ({ direction = 'top', itemData}) => {
+  return (
+    <div className="card-item-container">
+      <img src={itemData.image} alt='' className={`card-item-image ${direction}`} />
+      <p className="card-item-title text-primary">{itemData.title}</p>
+      <p className="card-item-subtitle text-secondary">{itemData.subtitle}</p>
+    </div>
+  )
+}
+
+const ImageCard = ({ direction = 'top', data }) => {
+  const isTop = direction === 'top';
+
+  return (
+    <div className={`card-container ${direction}`}>
+      {data.map((item, index) => (<CardItem key={index} itemData={item} />))}
+      <div className={`background-image ${direction}`}>
+        <img src={isTop ? TopImage : BottomImage} alt='back' />
+      </div>
+    </div>
+  )
+}
+
+export default ImageCard;
