848ac3371d7e07e16aeb45ca28f1069d649c988a
Alex <vanguard88728@gmail.com>
2023-02-03 11:05:09 +0800

    Update Investor section

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -161,6 +161,12 @@ const Home = () => {
       <section id='investors'>
         <div className="container section-container">
           <SectionTitle title='Investors' />
+          <ImageCard data={investorCardData} direction='bottom' />
+          <p className="section-content text-center my-40">
+            Our mission is to be one of the fastest growing restaurant <br/>companies and franchises, with portfolioof
+            leading <br/> and differentiated restaurant concepts.
+          </p>
+          <Divider />
         </div>
       </section>
 
