531a3a898695930b3305778d1058d56319fad2c4
Alex <vanguard88728@gmail.com>
2023-02-03 10:42:56 +0800

    Update Diversity section

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -17,6 +17,7 @@ import Card13 from '../../assets/images/Card1-3.png';
 import Card21 from '../../assets/images/Card2-1.png';
 import Card22 from '../../assets/images/Card2-2.png';
 import Card23 from '../../assets/images/Card2-3.png';
+import DiversityLeaf from '../../assets/images/DiversityLeaf.png';
 
 const businessCardData = [
   {
@@ -139,6 +140,21 @@ const Home = () => {
       <section id='diversity'>
         <div className="container section-container">
           <SectionTitle title='Diversity' />
+          <p className="section-content">
+            Our Principles & Beliefs, the guiding values at PROM RESOURCES INC, state that “We believe in the sanctity
+            of the individual, the value of diversity and inclusion, and in treating people with kindness, respect, and
+            understanding.” Our culture is founded on respecting and valuing every person, including but not limited to
+            differences in gender, race, ethnic origin, religion, sexual orientation, ability, or age.
+          </p>
+          <img src={DiversityLeaf} alt='' className="diversity-leaf" />
+          <p className="section-content">
+            As a hospitality & franchising company, the goal for our companies and our team is to cultivate a welcoming, safe, and
+            inclusive environment that celebrates diverse backgrounds and provides equitable access to opportunity.
+            We deliver on this by ensuring everyone is trained, understands, and is held accountable for the role they
+            play in making our restaurants a place where everyone is valued for who they are and what they bring to the table.
+          </p>
+          <div className="inline-divider" />
+          <Divider align='right' />
         </div>
       </section>
 
