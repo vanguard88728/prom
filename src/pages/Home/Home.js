e16174cd03315e605fc6ec80ec62069e4ecb9ec6
Alex <vanguard88728@gmail.com>
2023-02-03 14:09:59 +0800

    Update Contact section

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,3 +1,4 @@
+import { TextField } from '@mui/material';
 import Header from '../../components/Header/Header';
 import Navigator from '../../components/Navigator/Navigator';
 import ImageBanner from '../../components/ImageBanner/ImageBanner';
@@ -18,6 +19,10 @@ import Card21 from '../../assets/images/Card2-1.png';
 import Card22 from '../../assets/images/Card2-2.png';
 import Card23 from '../../assets/images/Card2-3.png';
 import DiversityLeaf from '../../assets/images/DiversityLeaf.png';
+import ContactBanner from '../../assets/images/ContactBanner.png';
+import ContactLeaf from '../../assets/images/ContactLeaf.png';
+import GoogleMap from '../../assets/images/map.png';
+import Submit from '../../assets/images/Submit.png';
 
 const businessCardData = [
   {
@@ -171,8 +176,45 @@ const Home = () => {
       </section>
 
       <section id='contact'>
+        <div className="contact-banner">
+          <img src={ContactBanner} alt='' className="banner-image" />
+          <img src={ContactLeaf} alt='leaf' className="contact-banner-leaf" />
+        </div>
         <div className="container section-container">
           <SectionTitle title='Contact Us' />
+          <div className="contact-wrapper">
+            <div className="contact-form">
+              <TextField 
+                fullWidth
+                placeholder='NAME'
+                className="form-input"
+              />
+              <TextField
+                fullWidth
+                placeholder='EMAIL'
+                className="form-input"
+              />
+              <TextField
+                fullWidth
+                placeholder='PHONE'
+                className="form-input"
+              />
+              <TextField
+                fullWidth
+                multiline
+                rows={4}
+                placeholder='MESSAGE'
+                className="form-input"
+              />
+              <div className="submit-button">
+                <img src={Submit} alt='submit' />
+                <p className="submit-button-text">Submit</p>
+              </div>
+            </div>
+            <div className="contact-map">
+              <img src={GoogleMap} alt='map' />
+            </div>
+          </div>
         </div>
       </section>
     </>
